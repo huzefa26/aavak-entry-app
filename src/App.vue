1495d56a329da9f36c56f1b812d883d6a59376f4
Huzefa Jambughoda <31165049+huzefa26@users.noreply.github.com>
2020-11-10 19:11:27 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="app" class="container">
+		<h1 class="heading mt-4">Entry</h1>
+		<hr>
+		<div class="row">
+			<div class="col">
+				<app-form></app-form>
+				<!-- <auto-complete :items="names"></auto-complete> -->
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import AutoComplete from './components/AutoComplete.vue';
+	import Form from './components/Form.vue';
+
+	export default {
+		name: 'App',
+		components: {
+			AutoComplete,
+			appForm: Form
+		},
+		data() {
+			return {
+				names: [
+					'Apple',
+					'Namna',
+					'Chetty',
+					'Dadum'
+				],
+				value: ''
+			}
+		}
+	};
+</script>
+
+<style>
+</style>
