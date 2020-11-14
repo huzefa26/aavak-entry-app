1a502559498a6412c4c80d83ffa82f199513765d
Huzefa Jambughoda <31165049+huzefa26@users.noreply.github.com>
2020-11-14 11:37:26 +0530

    Add files via upload

---
+++ src/components/Report.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="container">
+		<table>
+			<thead>
+				<tr>
+					<th></th>
+					<th v-for="type in types">{{ type }}</th>
+					<th>Total</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th>Bags</th>
+					<td v-for="type in types">{{ type }}</td>
+					<td>{{ 0 }}</td>
+				</tr>
+				<tr>
+					<th>Price</th>
+					<td v-for="type in types">{{ type }}</td>
+					<td>{{ 0 }}</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				types: this.$store.state.paddyTypes;
+			}
+		}
+	};
+</script>
+
